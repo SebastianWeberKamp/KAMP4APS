e837f6d8dd618ba084ed7da958de10eaf1f46131
Sandro Koch <sandro.koch@kit.edu>
2017-01-04 12:23:01 +0000

    amp

---
+++ edu.kit.ipd.sdq.amp.model.modificationmarks/src/edu/kit/ipd/sdq/amp/model/modificationmarks/ChangePropagationStep.java
@@ -0,0 +1,17 @@
+/**
+ */
+package edu.kit.ipd.sdq.amp.model.modificationmarks;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Change Propagation Step</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.kit.ipd.sdq.amp.model.modificationmarks.ModificationmarksPackage#getChangePropagationStep()
+ * @model
+ * @generated
+ */
+public interface ChangePropagationStep extends AbstractChangePropagationStep {
+} // ChangePropagationStep
