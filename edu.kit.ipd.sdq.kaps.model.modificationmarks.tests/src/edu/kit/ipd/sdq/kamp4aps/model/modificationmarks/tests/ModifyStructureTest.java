8fb35f44c2f1317539d4e49e3d96329b62b05000
Sandro Koch <sandro.koch@kit.edu>
2017-02-14 19:28:12 +0000

    20170214_2 typo

---
+++ edu.kit.ipd.sdq.kaps.model.modificationmarks.tests/src/edu/kit/ipd/sdq/kamp4aps/model/modificationmarks/tests/ModifyStructureTest.java
@@ -0,0 +1,55 @@
+/**
+ */
+package edu.kit.ipd.sdq.kamp4aps.model.modificationmarks.tests;
+
+import edu.kit.ipd.sdq.kamp4aps.model.modificationmarks.ModifyStructure;
+
+import junit.framework.TestCase;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Modify Structure</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class ModifyStructureTest extends TestCase {
+
+	/**
+	 * The fixture for this Modify Structure test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ModifyStructure<?> fixture = null;
+
+	/**
+	 * Constructs a new Modify Structure test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ModifyStructureTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Modify Structure test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(ModifyStructure<?> fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Modify Structure test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ModifyStructure<?> getFixture() {
+		return fixture;
+	}
+
+} //ModifyStructureTest
