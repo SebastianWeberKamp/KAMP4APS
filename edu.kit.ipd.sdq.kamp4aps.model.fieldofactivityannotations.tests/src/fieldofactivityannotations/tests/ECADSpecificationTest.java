35d1d1d48271026ae6f4eca4002226bb51f0c0ee
Sandro Koch <sandro.koch@kit.edu>
2017-03-03 10:48:46 +0000

    update foaa test

---
+++ edu.kit.ipd.sdq.kamp4aps.model.fieldofactivityannotations.tests/src/fieldofactivityannotations/tests/ECADSpecificationTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package fieldofactivityannotations.tests;
+
+import fieldofactivityannotations.ECADSpecification;
+import fieldofactivityannotations.FieldofactivityannotationsFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>ECAD Specification</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ECADSpecificationTest extends TestCase {
+
+	/**
+	 * The fixture for this ECAD Specification test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ECADSpecification fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(ECADSpecificationTest.class);
+	}
+
+	/**
+	 * Constructs a new ECAD Specification test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ECADSpecificationTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this ECAD Specification test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(ECADSpecification fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this ECAD Specification test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ECADSpecification getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(FieldofactivityannotationsFactory.eINSTANCE.createECADSpecification());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //ECADSpecificationTest
---
+++ edu.kit.ipd.sdq.kamp4aps.model.fieldofactivityannotations.tests/src/fieldofactivityannotations/tests/StructureStockListTest.java
@@ -0,0 +1,70 @@
+/**
+ */
+package fieldofactivityannotations.tests;
+
+import fieldofactivityannotations.FieldofactivityannotationsFactory;
+import fieldofactivityannotations.StructureStockList;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Structure Stock List</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class StructureStockListTest extends StockListTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(StructureStockListTest.class);
+	}
+
+	/**
+	 * Constructs a new Structure Stock List test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StructureStockListTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Structure Stock List test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected StructureStockList getFixture() {
+		return (StructureStockList)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(FieldofactivityannotationsFactory.eINSTANCE.createStructureStockList());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //StructureStockListTest
