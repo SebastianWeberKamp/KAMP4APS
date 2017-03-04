f4fea53dd8309a809a85d50c926d35c29c8f975e
Sandro Koch <sandro.koch@kit.edu>
2017-03-04 15:53:47 +0000

    20170304_1 feature stop

---
+++ edu.kit.ipd.sdq.kamp4aps.model.fieldofactivityannotations.tests/src/fieldofactivityannotations/tests/InterfaceStockListTest.java
@@ -0,0 +1,70 @@
+/**
+ */
+package fieldofactivityannotations.tests;
+
+import fieldofactivityannotations.FieldofactivityannotationsFactory;
+import fieldofactivityannotations.InterfaceStockList;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Interface Stock List</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class InterfaceStockListTest extends StockListTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(InterfaceStockListTest.class);
+	}
+
+	/**
+	 * Constructs a new Interface Stock List test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public InterfaceStockListTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Interface Stock List test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected InterfaceStockList getFixture() {
+		return (InterfaceStockList)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(FieldofactivityannotationsFactory.eINSTANCE.createInterfaceStockList());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //InterfaceStockListTest
