9a1560d1e69ddd87adf350e0a117a8781ee4fffc
Sandro Koch <sandro.koch@kit.edu>
2017-03-04 15:50:39 +0000

    add deploymentcontext projects

---
+++ edu.kit.ipd.sdq.kamp4aps.model.deploymentcontext.tests/src/DeploymentContext/tests/DeploymentcontextAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package DeploymentContext.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Deploymentcontext</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class DeploymentcontextAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new DeploymentcontextAllTests("Deploymentcontext Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public DeploymentcontextAllTests(String name) {
+		super(name);
+	}
+
+} //DeploymentcontextAllTests
