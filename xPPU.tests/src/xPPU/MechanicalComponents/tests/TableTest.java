d0d5a7546dcdf79a11fb7902193bc0d1706765c8
Sandro Koch <sandro.koch@kit.edu>
2017-06-06 13:38:19 +0200

    Rebuild xPPU

---
+++ xPPU.tests/src/xPPU/MechanicalComponents/tests/TableTest.java
@@ -0,0 +1,72 @@
+/**
+ */
+package xPPU.MechanicalComponents.tests;
+
+import junit.textui.TestRunner;
+
+import xPPU.ComponentRepository.tests.MechanicalAssemblyTest;
+
+import xPPU.MechanicalComponents.MechanicalComponentsFactory;
+import xPPU.MechanicalComponents.Table;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Table</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TableTest extends MechanicalAssemblyTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(TableTest.class);
+	}
+
+	/**
+	 * Constructs a new Table test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TableTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Table test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected Table getFixture() {
+		return (Table)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(MechanicalComponentsFactory.eINSTANCE.createTable());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //TableTest
