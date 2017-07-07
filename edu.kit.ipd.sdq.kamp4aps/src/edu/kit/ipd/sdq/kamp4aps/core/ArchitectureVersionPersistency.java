c54cb197beab944bfa32d5960bc809ab15ecc84f
Sandro Koch <sandro.koch@kit.edu>
2017-07-07 15:49:27 +0200

    impl. save

--- edu.kit.ipd.sdq.kamp4aps/src/edu/kit/ipd/sdq/kamp4aps/core/ArchitectureVersionPersistency.java
+++ edu.kit.ipd.sdq.kamp4aps/src/edu/kit/ipd/sdq/kamp4aps/core/ArchitectureVersionPersistency.java
@@ -6,7 +6,6 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import DeploymentContext.DeploymentContextRepository;
-//import de.uka.ipd.sdq.componentInternalDependencies.ComponentInternalDependencyRepository;
 import edu.kit.ipd.sdq.kamp.util.FileAndFolderManagement;
 import edu.kit.ipd.sdq.kamp4aps.core.ArchitectureVersion.ArchitectureVersionParams;
 import fieldofactivityannotations.FieldOfActivityAnnotationRepository;
@@ -14,60 +13,89 @@ import edu.kit.ipd.sdq.kamp4aps.model.modificationmarks.AbstractKAPSModification
 import edu.kit.ipd.sdq.kamp.architecture.AbstractArchitectureVersionPersistency;
 import xPPU.Plant;
 
-public class ArchitectureVersionPersistency extends AbstractArchitectureVersionPersistency<ArchitectureVersion> {	
-	
-	
+public class ArchitectureVersionPersistency extends AbstractArchitectureVersionPersistency<ArchitectureVersion> {
+
 	private ArchitectureVersionParams archParams = new ArchitectureVersionParams();
 	public static final String FILEEXTENSION_REPOSITORY = "repository";
 	public static final String FILEEXTENSION_SYSTEM = "system";
 	public static final String FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS = "fieldofactivityannotations";
 	public static final String FILEEXTENSION_XPPU = "xppu";
 	public static final String FILEEXTENSION_DEPLOYMENTCONTEXT = "deploymentcontext";
-	
+
 	@Override
 	public ArchitectureVersion load(String folderpath, String filename, String versionname) {
-		ResourceSet loadResourceSet = new ResourceSetImpl();	
-		String fieldOfActivityRepositoryFilePath = filename + "."+FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS;
+		ResourceSet loadResourceSet = new ResourceSetImpl();
+		String fieldOfActivityRepositoryFilePath = filename + "." + FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS;
 		String internalModFilePath = filename + "." + FILEEXTENSION_MODIFICATIONMARK;
 		String xppufilePath = filename + "." + FILEEXTENSION_XPPU;
-		
+
 		archParams.name = versionname;
-		archParams.fieldOfActivityRepository = (FieldOfActivityAnnotationRepository)loadEmfModelFromResource(folderpath, fieldOfActivityRepositoryFilePath, loadResourceSet);
-		archParams.modificationMarkRepository = (AbstractKAPSModificationRepository<?>)loadEmfModelFromResource(folderpath, internalModFilePath, loadResourceSet);
-		archParams.xPPUPlant = (Plant)loadEmfModelFromResource(folderpath, xppufilePath, loadResourceSet);
-		archParams.deploymentContextRepository = (DeploymentContextRepository)loadEmfModelFromResource(folderpath, internalModFilePath, loadResourceSet);
+		archParams.fieldOfActivityRepository = (FieldOfActivityAnnotationRepository) loadEmfModelFromResource(
+				folderpath, fieldOfActivityRepositoryFilePath, loadResourceSet);
+		archParams.modificationMarkRepository = (AbstractKAPSModificationRepository<?>) loadEmfModelFromResource(
+				folderpath, internalModFilePath, loadResourceSet);
+		archParams.xPPUPlant = (Plant) loadEmfModelFromResource(folderpath, xppufilePath, loadResourceSet);
+		archParams.deploymentContextRepository = (DeploymentContextRepository) loadEmfModelFromResource(folderpath,
+				internalModFilePath, loadResourceSet);
 		return new ArchitectureVersion(archParams);
-	}	
-	
+	}
+
 	@Override
 	public ArchitectureVersion load(IContainer folder, String versionname) {
-		ResourceSet loadResourceSet = new ResourceSetImpl();	
-		IFile fieldOfActivityRepositoryFile = FileAndFolderManagement.retrieveFileWithExtension(folder, FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS);
-		IFile internalModFile = FileAndFolderManagement.retrieveFileWithExtension(folder, FILEEXTENSION_MODIFICATIONMARK);
+		ResourceSet loadResourceSet = new ResourceSetImpl();
+		IFile fieldOfActivityRepositoryFile = FileAndFolderManagement.retrieveFileWithExtension(folder,
+				FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS);
+		IFile internalModFile = FileAndFolderManagement.retrieveFileWithExtension(folder,
+				FILEEXTENSION_MODIFICATIONMARK);
 		IFile xppufile = FileAndFolderManagement.retrieveFileWithExtension(folder, FILEEXTENSION_XPPU);
-		IFile deploymentContextFile = FileAndFolderManagement.retrieveFileWithExtension(folder, FILEEXTENSION_DEPLOYMENTCONTEXT);
-		
+		IFile deploymentContextFile = FileAndFolderManagement.retrieveFileWithExtension(folder,
+				FILEEXTENSION_DEPLOYMENTCONTEXT);
+
 		archParams.fieldOfActivityRepository = null;
 		archParams.modificationMarkRepository = null;
 		archParams.xPPUPlant = null;
 		archParams.deploymentContextRepository = null;
-		
+
 		archParams.name = versionname;
 		if (fieldOfActivityRepositoryFile != null && fieldOfActivityRepositoryFile.exists())
-			archParams.fieldOfActivityRepository = (FieldOfActivityAnnotationRepository)loadEmfModelFromResource(fieldOfActivityRepositoryFile.getFullPath().toString(), null, loadResourceSet);
+			archParams.fieldOfActivityRepository = (FieldOfActivityAnnotationRepository) loadEmfModelFromResource(
+					fieldOfActivityRepositoryFile.getFullPath().toString(), null, loadResourceSet);
 		if (internalModFile != null && internalModFile.exists())
-			archParams.modificationMarkRepository = (AbstractKAPSModificationRepository<?>)loadEmfModelFromResource(internalModFile.getFullPath().toString(), null, loadResourceSet);
+			archParams.modificationMarkRepository = (AbstractKAPSModificationRepository<?>) loadEmfModelFromResource(
+					internalModFile.getFullPath().toString(), null, loadResourceSet);
 		if (xppufile != null && xppufile.exists())
-			archParams.xPPUPlant = (Plant)loadEmfModelFromResource(xppufile.getFullPath().toString(), null, loadResourceSet);
+			archParams.xPPUPlant = (Plant) loadEmfModelFromResource(xppufile.getFullPath().toString(), null,
+					loadResourceSet);
 		if (deploymentContextFile != null && deploymentContextFile.exists())
-			archParams.deploymentContextRepository = (DeploymentContextRepository)loadEmfModelFromResource(deploymentContextFile.getFullPath().toString(), null, loadResourceSet);
+			archParams.deploymentContextRepository = (DeploymentContextRepository) loadEmfModelFromResource(
+					deploymentContextFile.getFullPath().toString(), null, loadResourceSet);
 		return new ArchitectureVersion(archParams);
 	}
 
 	@Override
 	public void save(String targetDirectoryPath, String filename, ArchitectureVersion version) {
-		// TODO Auto-generated method stub
-		
+		savePCMAndKAMP4APSModels(targetDirectoryPath, filename, version);
 	}
-	
+
+	public static void savePCMAndKAMP4APSModels(String targetDirectoryPath, String filename, ArchitectureVersion version) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		String internalModFilePath = filename + "." + FILEEXTENSION_MODIFICATIONMARK;
+		String fieldOfActivityRepositoryFilePath = filename + "." + FILEEXTENSION_FIELDOFACTIVITYANNOTATIONS;
+		String programsFilePath = filename + "." + FILEEXTENSION_DEPLOYMENTCONTEXT;
+		String xppuFilePath = filename + "." + FILEEXTENSION_XPPU;
+
+		if (version.getModificationMarkRepository() != null)
+			saveEmfModelToResource(version.getModificationMarkRepository(), targetDirectoryPath, internalModFilePath,
+					resourceSet);
+		if (version.getFieldOfActivityRepository() != null)
+			saveEmfModelToResource(version.getFieldOfActivityRepository(), targetDirectoryPath,
+					fieldOfActivityRepositoryFilePath, resourceSet);
+		if (version.getDeploymentContextRepository() != null)
+			saveEmfModelToResource(version.getDeploymentContextRepository(), targetDirectoryPath, programsFilePath,
+					resourceSet);
+		if (version.getXPPUPlant() != null)
+			saveEmfModelToResource(version.getXPPUPlant(), targetDirectoryPath, xppuFilePath,
+					resourceSet);
+	}
+
 }
