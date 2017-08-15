6b23285f7ad0e6726b883dec045378855f3d0b9e
Sandro Koch <sandro.koch@kit.edu>
2017-08-15 13:18:01 +0200

    Modificationmarks Metamodel: ModifyMicroSwitch<Component> => ModifyMicroSwitch<Module>

---
+++ edu.kit.ipd.sdq.kamp4aps.model.modificationmarks/src/edu/kit/ipd/sdq/kamp4aps/model/modificationmarks/ModifyEntity.java
@@ -0,0 +1,22 @@
+/**
+ */
+package edu.kit.ipd.sdq.kamp4aps.model.modificationmarks;
+
+import edu.kit.ipd.sdq.kamp.model.modificationmarks.AbstractModification;
+
+import org.eclipse.emf.ecore.EObject;
+
+import xPPU.Entity;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Modify Entity</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.kit.ipd.sdq.kamp4aps.model.modificationmarks.ModificationmarksPackage#getModifyEntity()
+ * @model abstract="true"
+ * @generated
+ */
+public interface ModifyEntity<T extends Entity> extends AbstractModification<T, EObject> {
+} // ModifyEntity
